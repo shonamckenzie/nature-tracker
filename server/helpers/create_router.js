2e0bd85ecbc6376304e248d8f3fbd85c136bd212
Michael Alexander <michael_alexander_20@outlook.com>
2019-09-20 16:17:07 +0100

    get function added

---
+++ server/helpers/create_router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+
+const createRouter = function(collection){
+
+  const router = express.Router();
+
+// INDEX
+
+  router.get('/', (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+      });
+  });
+
+
+// SHOW
+
+
+
+// UPDATE
+
+
+
+
+
+}
