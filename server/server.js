f73956a2f2a38350998bad95c65fccc3a5ad132a
Michael Alexander <michael_alexander_20@outlook.com>
2019-09-20 16:05:20 +0100

    more server.js work

--- server/server.js
+++ server/server.js
@@ -10,3 +10,16 @@ app.use(parser.json());
 const MongoClient = require('mongodb').MongoClient;
 
 const createRouter = require('./helpers/create_router');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('wildlife');
+  const speciesCollection = db.collection('species');
+  const speciesRouter = createRouter(speciesCollection);
+  app.use('/api/species', speciesRouter);
+})
+.catch(console.err);
+
+app.listen(3000, function(){
+  console.log(`listening on port ${this.address().port}`);
+});
