aca195f968ad6b1ca0eac8ae76d22b9cf4f766e9
Joe Cooper <josephcooper3@gmail.com>
2019-09-21 16:03:05 +0100

    adds placeholders

--- client/src/components/SpeciesList.vue
+++ client/src/components/SpeciesList.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="species-list">
     <div class="species" v-for="species in allSpecies" :key="species.name">
+      <!-- This div is just a placeholder - to be replaced by species-card component -->
       <p>{{ species.name }}</p>
     </div>
   </div>
@@ -11,7 +12,12 @@ import SpeciesService from '../services/SpeciesService';
 
 export default {
   name: 'species-list',
-  props: ['allSpecies']
+  props: ['allSpecies'],
+  components: {
+    // 'species-card': SpeciesCard,
+    // 'add-sighting-form': AddSightingForm
+    // Uncomment when these are added
+  }
 }
 </script>
 
