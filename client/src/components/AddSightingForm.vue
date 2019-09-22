e51cd7986fe72d84e4bbef01ba365df7e958e523
Joe Cooper <josephcooper3@gmail.com>
2019-09-22 21:23:22 +0100

    adds data and v-models for form

--- client/src/components/AddSightingForm.vue
+++ client/src/components/AddSightingForm.vue
@@ -1,25 +1,25 @@
 <template>
-  <form>
+  <form v-on:submit="addSighting" method="post">
     <h3>Add a New Sighting</h3>
     <div>
       <label for="species">Species:</label>
-      <input type="text" />
+      <input type="text" v-model="species" required />
     </div>
     <div>
       <label for="date">Date Sighted:</label>
-      <input type="text" />
+      <input type="date" v-model="date" required />
     </div>
     <div>
       <label for="location">Location Sighted:</label>
-      <input type="text" />
+      <input type="text" v-model="location" required />
     </div>
     <div>
       <label for="locationLat">Location Latitude:</label>
-      <input type="text" />
+      <input type="number" v-model="locationLat" />
     </div>
     <div>
       <label for="locationLon">Location Longitude:</label>
-      <input type="text" />
+      <input type="number" v-model="locationLon" />
     </div>
   </form>
 </template>
@@ -27,7 +27,21 @@
 <script>
 export default {
   name: "add-sighting-form",
-  props: ["allSpecies"]
+  props: ["allSpecies"],
+  data() {
+    return {
+      species: "",
+      date: null,
+      location: "",
+      locationLat: null,
+      locationLon: null
+    }
+  },
+  methods: {
+    addSighting(event) {
+
+    }
+  }
 };
 </script>
 
