a91105445239ec0fd2847ce56ecb15773fb0be7d
Joe Cooper <josephcooper3@gmail.com>
2019-09-22 21:38:15 +0100

    adds drop-down list of species

--- client/src/components/AddSightingForm.vue
+++ client/src/components/AddSightingForm.vue
@@ -2,24 +2,26 @@
   <form v-on:submit="addSighting" method="post">
     <h3>Add a New Sighting</h3>
     <div>
-      <label for="species">Species:</label>
-      <input type="text" v-model="species" required />
+      <label for="name">Species:</label>
+      <select id="name" v-model="id" required >
+        <option v-for="species in allSpecies" :value="species._id" :key="species.name">{{ species.name }}</option>
+      </select>
     </div>
     <div>
       <label for="date">Date Sighted:</label>
-      <input type="date" v-model="date" required />
+      <input type="date" id="date" v-model="date" required />
     </div>
     <div>
       <label for="location">Location Sighted:</label>
-      <input type="text" v-model="location" required />
+      <input type="text" id="location" v-model="location" required />
     </div>
     <div>
       <label for="locationLat">Location Latitude:</label>
-      <input type="number" v-model="locationLat" />
+      <input type="number" id="locationLat" v-model="locationLat" />
     </div>
     <div>
       <label for="locationLon">Location Longitude:</label>
-      <input type="number" v-model="locationLon" />
+      <input type="number" id="locationLon" v-model="locationLon" />
     </div>
   </form>
 </template>
@@ -30,7 +32,7 @@ export default {
   props: ["allSpecies"],
   data() {
     return {
-      species: "",
+      id: null,
       date: null,
       location: "",
       locationLat: null,
