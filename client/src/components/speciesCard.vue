23062ac3a3f9f6cae2e9e893407cce338508ca84
Michael Alexander <michael_alexander_20@outlook.com>
2019-09-22 13:05:04 +0100

    begin speciesCard structure

---
+++ client/src/components/speciesCard.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+<div class="species-card">
+  <h3>{{ species.name }}</h3>
+  <p>Description: </p>
+  <p>Habitat: </p>
+  <p>Sightings: </p>
+</div>
+</template>
+
+<script>
+import { eventBus } from '@/main.js'
+import SpeciesService from '@/services/SpeciesService.js'
+export default {
+  name: 'species-card',
+  props: ['species'],
+  methods: {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
