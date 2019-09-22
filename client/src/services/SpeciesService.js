1e43c3251524d1bdf2ca49d30a0ed9248e82dbc7
Joe Cooper <josephcooper3@gmail.com>
2019-09-22 21:50:43 +0100

    adds SpeciesService.updateSpecies method

--- client/src/services/SpeciesService.js
+++ client/src/services/SpeciesService.js
@@ -7,4 +7,13 @@ export default {
     .then(res => res.json())
   },
 
+  updateSpecies(id, payload){
+    return fetch(baseURL + id, {
+      method: "PUT",
+      body: JSON.stringify(payload),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  }
+
 }
