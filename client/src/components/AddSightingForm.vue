563b1e34205ceea2638f39e00b826c29b5609330
Joe Cooper <josephcooper3@gmail.com>
2019-09-22 20:56:15 +0100

    adds form fields and labels

--- client/src/components/AddSightingForm.vue
+++ client/src/components/AddSightingForm.vue
@@ -1,14 +1,35 @@
 <template>
-  
+  <form>
+    <h3>Add a New Sighting</h3>
+    <div>
+      <label for="species">Species:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="date">Date Sighted:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="location">Location Sighted:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="locationLat">Location Latitude:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="locationLon">Location Longitude:</label>
+      <input type="text" />
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'add-sighting-form',
-  props: ['allSpecies']
-}
+  name: "add-sighting-form",
+  props: ["allSpecies"]
+};
 </script>
 
 <style>
-
 </style>
