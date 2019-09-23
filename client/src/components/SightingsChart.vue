117129b5d091a089dcd0aead3bf08de1c8258234
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-23 17:52:37 +0100

    get highcharts working dynamically

--- client/src/components/SightingsChart.vue
+++ client/src/components/SightingsChart.vue
@@ -1,64 +1,50 @@
 <template>
   <div class="chart">
-      <highcharts id="barChart" :options="chartOptions"></highcharts>
+    <highcharts id="barChart" :options="chartOptions"></highcharts>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            chartOptions:{
-                title: {
-                    text: 'Sightings by Species'
-                },
-                chart: {
-                    type: 'column'
-                },
-                plotOptions: {
-                    column: {
-                    pointPadding: 0,
-                    borderWidth: 5,
-                    groupPadding: 0,
-                    shadow: false
-                     }
-                },
-                xAxis: {
-                    categories: ['Badger', 'Hedgehog', 'Red Deer']
-                },
-                 yAxis: {
-                    title: {
-                        text: 'Sightings'
-                     }
-                },
-                 series: [{
-                     name: 'Species',
-                     data: [{
-                        color: '#00FF00',
-                        y: 1
-                    }, {
-                        color: '#FF00FF',
-                        y: 5
-                    }, {  
-                        color: 'black',
-                        y: 8  
-                    }]
-                 }]
-            }
-        }
-    },
-    props: ['allSpecies'],
-    computed: {
-        speciesNames: function () {
-           return this.allSpecies.map(species => species.name)
+  computed: {
+    chartOptions() {
+      return {
+        title: {
+          text: "Sightings by Species"
         },
-        sightingsCounts: function () {
-            return this.allSpecies.map(species => species.sightings.length)
-        }
+        chart: {
+          type: "column"
+        },
+        plotOptions: {
+          column: {
+            pointPadding: 0,
+            borderWidth: 5,
+            groupPadding: 0,
+            shadow: false
+          }
+        },
+        xAxis: {
+          categories: this.allSpecies.map(species => species.name)
+        },
+        yAxis: {
+          title: {
+            text: "Sightings"
+          }
+        },
+        series: [
+                {
+                    name: 'Total Sightings',
+                    data: this.allSpecies.map(species => species.sightings.length)
+                }
+        ]
+      }
     }
-}
+  },
+  props: ["allSpecies"]
+};
+
+
 </script>
 
 <style>
-
 </style>
