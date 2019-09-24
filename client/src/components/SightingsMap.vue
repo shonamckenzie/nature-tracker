c3385171bdc8e069f84f80f26ae5e3eba9b37299
Joe Cooper <josephcooper3@gmail.com>
2019-09-24 16:03:09 +0100

    displays map of Scotland

--- client/src/components/SightingsMap.vue
+++ client/src/components/SightingsMap.vue
@@ -1,17 +1,37 @@
 <template>
-  <div id="sightings-map">
-
-  </div>
+  <div id="sightings-map"></div>
 </template>
 
 <script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+const key = require("../map-key");
+
 export default {
-  props: ['speciesOptions']
-}
+  name: "sightings-map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+  mounted() {
+    const sightingsMap = L.map("sightings-map").setView([56.5, -4], 6);
+    L.tileLayer(
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox.streets",
+        accessToken: key.token
+      }
+    ).addTo(sightingsMap);
+  }
+};
 </script>
 
 <style>
 #sightings-map {
-  height: 200px
+  height: 400px;
+  width: 400px;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -9,6 +9,17 @@ export const eventBus = new Vue();
 
 Vue.use(HighchartsVue);
 
+import { Icon }  from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
