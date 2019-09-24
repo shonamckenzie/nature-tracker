76f24e791250ded2d5da605301dadfe7b9153b7c
Joe Cooper <josephcooper3@gmail.com>
2019-09-24 11:16:09 +0100

    moves from to App instead of Species List

--- client/src/components/SpeciesList.vue
+++ client/src/components/SpeciesList.vue
@@ -3,21 +3,18 @@
     <div class="species" v-for="species in allSpecies" :key="species.name">
       <species-card :species="species"></species-card>
     </div>
-    <add-sighting-form :speciesOptions="allSpecies"/>
   </div>
 </template>
 
 <script>
 import SpeciesService from '../services/SpeciesService';
-import AddSightingForm from './AddSightingForm';
 import SpeciesCard from './SpeciesCard';
 
 export default {
   name: 'species-list',
   props: ['allSpecies'],
   components: {
-    'species-card': SpeciesCard,
-    'add-sighting-form': AddSightingForm
+    'species-card': SpeciesCard
   }
 }
 </script>
