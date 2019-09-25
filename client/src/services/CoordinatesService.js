a32fe017a5478e8df7f7042cfb8293fbab1498e0
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-09-25 07:02:27 +0100

    Api key now hidden in non git file

--- client/src/services/CoordinatesService.js
+++ client/src/services/CoordinatesService.js
@@ -1,14 +1,17 @@
+import config from "../config";
+
 export default {
   getCoord(location){
 
-    const url=`https://eu1.locationiq.com/v1/search.php?key=306bf02320ef63&q=${location},scotland&format=json`;
+    const mykey = config.API_KEY;
+
+    const url=`https://eu1.locationiq.com/v1/search.php?key=${mykey}&q=${location},scotland&format=json`;
 
     return fetch(url)
       .then(coords => coords.json())
       .then(coordData => {
-
-
         const locationData = coordData[0]
+        // console.log(locationData);
         return {
           lat: locationData.lat,
           lon: locationData.lon
